8d47df0cf5891f1b6fd5c5971dc4a3301c22f4df
home1303 <s6404062610031@email.kmutnb.ac.th>
2023-07-25 18:48:15 +0700

    descliption

---
+++ lab2html/lab2.3/lab2.3_finish.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="/lab2.3/style.css">
+</head>
+<body>
+
+    <h1>...เข้าสู่ระบบแล้ว...</h1>
+    <a href="/lab2.3/lab2.3.html">
+    <button>BACK</button>
+    </a>
+
+
+</body>
+</html>
