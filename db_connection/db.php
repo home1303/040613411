d2df9172abba68baa0c406b91cce9922bf9dcf48
home1303 <s6404062610031@email.kmutnb.ac.th>
2023-09-26 20:50:29 +0700

    descliption

---
+++ db_connection/db.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border ='1'><tr><th>รหัสสินค้า</th><th>ซื้อสินค้า</th><th>รายละเอียด</th><th>ราคา</th></tr>
+    <tr><td>1</td><td>Centrum</td><td>วิตามินรวมจาก A ถึง Zinc</td><td>350 บาท </td></th>
+    <tr><td>2</td><td>Caltrate</td><td>บำรุงกระดูก เสริมวิตามินดี</td><td>760 บาท </td></th>
+    <tr><td>3</td><td>Ester</td><td>วิตามินซี 500 mg ไทม่กัดกระเพาะ</td><td>500 บาท </td></th>
+    <tr><td>4</td><td>Glucosamine</td><td>บำรุงข้อต่อ ป้องกันข้อเสือม</td><td>1200 บาท </td></th>
+</table>
+
+</body>
+</html>
